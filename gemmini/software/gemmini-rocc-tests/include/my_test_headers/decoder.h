33d7dda48f736ed0b7cca10f67f889982d65add5
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-07-08 17:16:27 +0800

    reconstructed the program

---
+++ gemmini/software/gemmini-rocc-tests/include/my_test_headers/decoder.h
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "math.h"  // BUGGY
+
+#ifndef BAREMETAL
+#include <sys/mman.h>
+#endif // system func
+
+#include "include/gemmini_testutils.h"
+#include "include/gemmini_nn.h"
+#include "./auxiliary.h"
+
+
+void decoder()
+{
+  return;
+}
