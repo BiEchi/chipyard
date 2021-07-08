e31f15f91909aeb7c1ba93ec7ee1fd30ae160204
lyon <liyang.19@intl.zju.edu.cn>
2021-07-08 18:17:58 +0800

    correct aux

--- gemmini/software/gemmini-rocc-tests/include/my_test_headers/auxiliary.h
+++ gemmini/software/gemmini-rocc-tests/include/my_test_headers/auxiliary.h
@@ -6,7 +6,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "math.h"  // BUGGY
+#include "math.h" // BUGGY
 
 #ifndef BAREMETAL
 #include <sys/mman.h>
@@ -16,43 +16,43 @@
 #include "include/gemmini_nn.h"
 #include "./parameters.h"
 
-void print_matrix(size_t rows,size_t cols, elem_t mat[rows][cols])
+void print_matrix(size_t rows, size_t cols, elem_t mat[rows][cols])
 {
-  for(size_t i=0;i<rows;i++)
+  for (size_t i = 0; i < rows; i++)
   {
-    for(size_t j=0;j<cols;j++)
+    for (size_t j = 0; j < cols; j++)
     {
-      printf("%d ",mat[i][j]);
+      printf("%d ", mat[i][j]);
     }
     printf("\n");
   }
 }
 
-void rand_matrix(size_t rows,size_t cols, elem_t* mat)
+void rand_matrix(size_t rows, size_t cols, elem_t *mat)
 {
-  for(size_t i=0;i<rows;i++)
+  for (size_t i = 0; i < rows; i++)
   {
-    for(size_t j=0;j<cols;j++)
+    for (size_t j = 0; j < cols; j++)
     {
-      mat[i*cols+j] = rand()%10;
+      mat[i * cols + j] = rand() % 10;
     }
   }
 }
 
-void softmax(size_t rows,size_t col, elem_t* mat)
+void softmax(size_t rows, size_t col, elem_t *mat)
 {
-  elem_t tempsum=0;
+  elem_t tempsum = 0;
   float outmat[rows][col];
-  for(size_t i=0;i<rows;i++)
+  for (size_t i = 0; i < rows; i++)
   {
-    tempsum=0;
-    for(size_t j=0;j<col;j++)
+    tempsum = 0;
+    for (size_t j = 0; j < col; j++)
     {
-      tempsum+=0;
+      tempsum += 0;
     }
-    for(size_t j=0;j<col;j++)
+    for (size_t j = 0; j < col; j++)
     {
-      printf("%i",5);
+      printf("%i", 5);
     }
     printf("\n");
   }
@@ -60,46 +60,80 @@ void softmax(size_t rows,size_t col, elem_t* mat)
 
 long fac(int n)
 {
-  if(n==0)
+  if (n == 0)
   {
     return 1;
   }
   else
   {
     long temp = 1;
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-      temp=temp*i;
+      temp = temp * i;
     }
     return temp;
   }
 }
 
-long power(long x,int n)
+long power(long x, int n)
 {
   long temp = x;
-  if(n==0)
+  if (n == 0)
   {
     return 1;
   }
   else
   {
-    for(int i=1;i<n;i++)
+    for (int i = 1; i < n; i++)
     {
-      temp=temp*x;
+      temp = temp * x;
     }
     return temp;
   }
 }
 
-long exp_cal(int n,long x)
+long exp_cal(int n, long x)
 {
   float partial = 0;
-  for(int i=0;i<=n;i++)
+  for (int i = 0; i <= n; i++)
   {
-    partial+=power(x,i)/fac(i);
+    partial += power(x, i) / fac(i);
   }
   return partial;
+}
+
+void positional_embedding(elem_t word_mat[length][dimension])
+{
+  // word_mat = (int **)malloc(length*sizeof(elem_t*));
+  // for (int i = 0; i < length; i++)
+  // {
+  //   word_mat[i] = (int*)malloc(dimension*sizeof(elem_t));
+  // }
+  // 生成位置矩阵
+  if (dimension % 2 == 0)
+  {
+    for (int pos = 0; pos < length; pos++)
+    {
+      for (int i = 0; i < dimension; i += 2)
+      {
+        word_mat[pos][i] += sin(pos / pow(10000, 2 * i / dimension));
+        word_mat[pos][i + 1] += cos(pos / pow(10000, (2 * i + 1) / dimension));
+      }
+    }
+  }
+  else
+  {
+    for (int pos = 0; pos < length; pos++)
+    {
+      for (int i = 0; i < dimension - 1; i += 2)
+      {
+        word_mat[pos][i] += sin(pos / pow(10000, 2 * i / dimension));
+        word_mat[pos][i + 1] += cos(pos / pow(10000, (2 * i + 1) / dimension));
+      }
+      word_mat[pos][dimension-1] += sin(pos/pow(10000,2*(dimension - 1)/dimension));
+    }
+  }
+  return;
 }
 
 #endif /* AUX_H */
--- gemmini/software/gemmini-rocc-tests/include/my_test_headers/parameters.h
+++ gemmini/software/gemmini-rocc-tests/include/my_test_headers/parameters.h
@@ -1,4 +1,9 @@
+#ifndef PARAMETERS_H
+#define	PARAMETERS_H
 #define DIM_I 64
 #define DIM_J 32
 #define DIM_K 128
 #define n_head 4
+
+
+#endif
